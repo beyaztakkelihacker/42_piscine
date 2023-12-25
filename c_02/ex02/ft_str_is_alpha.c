646eb01822be77e8654d301e4b4ef292b455a71a
karatakkelihacker <151129176+karatakkelihacker@users.noreply.github.com>
2023-12-25 09:43:01 +0300

    Update ft_str_is_alpha.c

--- c_02/ex02/ft_str_is_alpha.c
+++ c_02/ex02/ft_str_is_alpha.c
@@ -1,6 +1,9 @@
-int ft_str_is_alpha(char *str) {
-    while (*str) {
-        if (!isalpha(*str)) {
+int ft_str_is_alpha(char *str) 
+{
+    while (*str)
+    {
+        if (!isalpha(*str))
+        {
             return 0;
         }
         str++;
