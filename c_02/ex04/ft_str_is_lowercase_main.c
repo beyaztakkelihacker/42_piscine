d37b4100f37d2602fe91ef55414fb4bf1e5d8825
karatakkelihacker <151129176+karatakkelihacker@users.noreply.github.com>
2023-12-25 10:00:17 +0300

    Update ft_str_is_lowercase_main.c

--- c_02/ex04/ft_str_is_lowercase_main.c
+++ c_02/ex04/ft_str_is_lowercase_main.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+#include <ctype.h>
 
-// Bir dizide sadece küçük harf karakterlerinin olup olmadığını kontrol eden fonksiyon
-int ft_str_is_lowercase(char *str) {
-    while (*str) {
-        // Karakter, 'a' ile 'z' aralığında değilse
-        if (*str < 'a' || *str > 'z') {
+int ft_str_is_lowercase(char *str) 
+{
+    while (*str) 
+    {
+        // Karakter, küçük harf değilse
+        if (!islower(*str)) 
+        {
             return 0; // 0 döndür ve işlemi sonlandır (küçük harf dışında bir karakter bulundu)
         }
         str++; // Bir sonraki karaktere geç
@@ -12,10 +15,12 @@ int ft_str_is_lowercase(char *str) {
     return 1; // Fonksiyonun bu noktaya ulaşması, sadece küçük harf karakterleri içerildiği anlamına gelir
 }
 
-int main() {
+int main()
+{
+    // Fonksiyonu çeşitli girdilerle test et
     printf("%d\n", ft_str_is_lowercase("abcdefghijkl"));     // 1 (sadece küçük harf karakterleri içeriyor)
     printf("%d\n", ft_str_is_lowercase("abcAdefghijkl"));     // 0 (küçük harf olmayan bir karakter içeriyor)
     printf("%d\n", ft_str_is_lowercase("-_134556efSghij67")); // 0 (küçük harf dışında karakterler içeriyor)
 
-    return 0;
+    return 0; // Programın başarıyla tamamlandığını belirtmek için 0 döndür
 }
