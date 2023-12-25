d09c0447e5a209dfc7851187f37bf5575940d6a7
karatakkelihacker <151129176+karatakkelihacker@users.noreply.github.com>
2023-12-25 10:06:07 +0300

    Update ft_str_is_printable_main.c

--- c_02/ex06/ft_str_is_printable_main.c
+++ c_02/ex06/ft_str_is_printable_main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
+#include <ctype.h>
 
 // Bir dizide sadece yazdırılabilir karakterlerin olup olmadığını kontrol eden fonksiyon
-int ft_str_is_printable(char *str) {
-    int i = 0;
-
-    while (str[i]) {
-        // Karakter, ASCII tablosundaki 32 (boşluk) ile 126 (tilde) aralığında değilse
-        if (str[i] < 32 || str[i] > 126) {
+int ft_str_is_printable(char *str) 
+{
+    while (*str)
+    {
+        // Karakter, yazdırılabilir bir karakter değilse
+        if (!isprint(*str)) 
+        {
             return 0; // 0 döndür ve işlemi sonlandır (yazdırılabilir olmayan bir karakter bulundu)
         }
-        i++; // Bir sonraki karaktere geç
+        str++; // Bir sonraki karaktere geç
     }
 
     return 1; // Fonksiyonun bu noktaya ulaşması, sadece yazdırılabilir karakterler içerildiği anlamına gelir
 }
 
-int main() {
+int main() 
+{
+    // Fonksiyonu test et
     printf("%d\n", ft_str_is_printable("ABDELKFSCO?I340990%")); // 1 (sadece yazdırılabilir karakterler içeriyor)
     printf("%d\n", ft_str_is_printable("\n\t\v\f")); // 0 (yazdırılabilir olmayan karakterler içeriyor)
 
