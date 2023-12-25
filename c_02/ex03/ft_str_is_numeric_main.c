96202e884b4c2947a435822d393d1fe0e330d658
karatakkelihacker <151129176+karatakkelihacker@users.noreply.github.com>
2023-12-25 09:50:06 +0300

    Update ft_str_is_numeric_main.c

--- c_02/ex03/ft_str_is_numeric_main.c
+++ c_02/ex03/ft_str_is_numeric_main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
 
 // Bir dizide sadece sayısal karakterlerin olup olmadığını kontrol eden fonksiyon
 int ft_str_is_numeric(char *str) 
 {
     while (*str) 
     {
-        // Karakter, '0' ile '9' aralığında değilse
-        if (!(*str >= '0' && *str <= '9')) 
+        if (!isdigit(*str)) 
 	{
-            return 0; // 0 döndür ve işlemi sonlandır (sayısal olmayan karakter bulundu)
+            return 0; // Sayısal olmayan bir karakter bulunduğunda 0 döndür ve işlemi sonlandır
         }
-        str++; // Bir sonraki karaktere geç
+        str++;
     }
     return 1; // Fonksiyonun bu noktaya ulaşması, sadece sayısal karakterler içerildiği anlamına gelir
 }
 
-int main() 
+int main()
 {
     printf("%d\n", ft_str_is_numeric("05092000")); // 1 (sadece sayısal karakterler içeriyor)
     printf("%d\n", ft_str_is_numeric("0509beyaz2000")); // 0 (sayısal olmayan bir karakter içeriyor)
