da7882c8d5ecfbed058f836fdc1dc109338281d4
Ömer Faruk Kuzu <144312116+beyaztakkelihacker@users.noreply.github.com>
2023-12-27 19:22:05 +0300

    Delete c_03 directory

--- c_03/ex01/ft_strncmp.c
+++
@@ -1,5 +0,0 @@
-int ft_strncmp(const char *s1, const char *s2, unsigned int n) {
-    unsigned int i = 0; // i'yi burada tanımla
-    for (; i < n && s1[i] && s1[i] == s2[i]; i++); // for döngüsü içinde ifadeyi düzelt
-    return (unsigned char)s1[i] - (unsigned char)s2[i];
-}
--- c_03/ex02/ft_strcat.c
+++
@@ -1,6 +0,0 @@
-char *ft_strcat(char *dest, const char *src) {
-    char *result = dest;
-    while (*dest) dest++;
-    while ((*dest++ = *src++));
-    return result;
-}
